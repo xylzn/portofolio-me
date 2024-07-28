d04f79c054c7b0edb62430846bb6d5064a93cb4a
Pasha Malik <pashamalik9371@gmail.com>
2024-07-29 01:12:55 +0700

    fix parallax

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+      container:{
+          center: true,
+          padding: '16px',
+      },
+      extend: {
+    colors: {
+        utama: '#F59E0B', gelap: '#111827',abu: '#9CA3AF'
+    },
+    screens:{
+      '2xl':  '1320px',
+    },
+    
+  }
+}
+}
