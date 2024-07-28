c0d8ec6783c96c40aa533eedb607132f5a23b6c1
Dedi Rialdi As Aldi <143459592+xylzn@users.noreply.github.com>
2024-07-28 23:06:07 +0700

    Add files via upload

---
+++ My Portfolio/Dist/js/script.js
@@ -0,0 +1,43 @@
+// nvbar-fixed
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const fixednav = header.offsetTop;
+
+    if(window.scrollY > fixednav) {
+        header.classList.add('navbar-fixed');
+    } else{
+        header.classList.remove('navbar-fixed');
+    }
+}
+
+// Burger
+const burger = document.querySelector('#burger');
+const navMenu =  document.querySelector('#nav-menu');
+
+burger.addEventListener('click', function(){
+    burger.classList.toggle('burger-active');
+    navMenu.classList.toggle('hidden');
+})
+
+// Form
+const contactForm = document.getElementById("contact-form");
+
+contactForm.addEventListener("submit", function(e)
+{
+    e.preventDefault();
+    const url = e.target.action;
+    const formData = new FormData(contactForm);
+
+    fetch(url, {
+        method: "POST",
+        body: formData,
+        mode:"no-cors",
+    })
+        .then(() => {
+        // url thank u
+        window.location.href = "/contact.html";
+        })
+        .catch((e) => alert("Error"));
+    
+
+});
---
+++ My Portfolio/Dist/particles.js-master/app.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', '/Dist/particles.js-master/particle.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
